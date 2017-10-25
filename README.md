eebc06b082021b9dd5ffe68d10cb93e1804d38b7
Hanqiu Jiang <hanq.jiang@gmail.com>
2017-10-26 01:20:35 +0200

    doc: Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+## Basic Algorithms in Python
+
+### Merge Sort
+O[n*log(n)]
+  
+<img src="./doc/merge_sort.png" width="500px">
--- benchmarking.py
+++ benchmarking.py
@@ -7,18 +7,27 @@ import time
 
 def benchmarking(sort_func):
 	benchmark_times = []
-	n_list = [] 
+	n_list = []
+
+	#expected_time
+	#for i in range(150):
+	#	n = 1000*(i+1)
+
+	tic = time.clock()
 	for i in tqdm(range(150)):
 		n = 1000*(i+1)
 		unsorted_list = [random.randint(0, n) for i in range(n)]
-		tic = time.clock()
+		tic2 = time.clock()
 		sorted_list = sort_func(unsorted_list)
-		benchmark_times.append(time.clock()-tic)
+		benchmark_times.append(time.clock()-tic2)
 		n_list.append(n)
+	print tic - time.clock()
 	return n_list, benchmark_times
 
 n_list, benchmark_times = benchmarking(merge_sort)
 
 plt.figure()
+plt.title('Merge Sort')
 plt.plot(n_list, benchmark_times)
+plt.savefig('./doc/merge_sort', bbox_inches='tight')
 plt.show()
